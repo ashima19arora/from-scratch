ff80ea365b4939ad297046f25a6a9e6d55eeda4d
Ashima Arora <arora.19ashima@gmail.com>
2026-08-31 05:24:20 +0530

    delete(at any pos/by value), length of LL

--- HeaderFiles/LinkedList.h
+++ HeaderFiles/LinkedList.h
@@ -2,7 +2,7 @@
 using namespace std;
 
 #include "ListNode.h"  //same folder
-// Section 01- insertion,printing ,deletion
+// Section 01- insertion,deletion
 //  we are going to use list node in this IAH function , so this function also
 //  must be templatized
 template <typename T>
@@ -13,42 +13,6 @@ void insertAtHead(ListNode<T>*& head, int data) {
   //*newNode is actually the new Node(val + address)
   (*newNode).next = head;
   head = newNode;
-}
-template <typename T>
-void printLL(ListNode<T>*& head) {
-  ListNode<int>* temp = head;
-  while (temp != nullptr) {
-    cout << temp->val << "-->";
-    temp = temp->next;
-  }
-  cout << "NULL" << endl;
-}
-
-// searching iteratively
-template <typename T>
-bool searchInLL(ListNode<T>*& head, T key) {
-  ListNode<int>* temp = head;
-  while (temp != nullptr) {
-    if (temp->val == key) {
-      return true;
-    }
-    temp = temp->next;
-  }
-  return false;
-}
-
-// searching recursively
-template <typename T>
-bool searchInLLRecursively(ListNode<T>* start, T key) {
-  if (start == nullptr) {
-    return false;
-  }
-  // current state f(i,key)
-  if (start == nullptr) {
-    return true;
-  }
-  start = start->next;
-  return searchInLLRecursively(start, key);
 }
 
 template <typename T>
@@ -95,4 +59,140 @@ void deleteAtHead(ListNode<T>*& head) {  // pass by reference-here u want to
   ListNode<T>* temp = head;
   head = head->next;
   delete temp;  // deallocate the memory temp is pointing towards
+}
+
+template <typename T>
+void deleteAtTail(ListNode<T>*& head) {  // pass by reference-here u want to
+                                         // reflect the changes
+  if (head == nullptr) {                 // LL is of 0 length
+    return;
+  }
+
+  ListNode<T>* temp = head;
+  if (head->next == nullptr) {  // LL is of 1 length
+    delete head;
+    head = nullptr;
+    return;
+  }
+
+  while (temp->next->next != null) {  // temp->next is a pointer
+    temp = temp->next;
+  }
+  // we are at the second last node;
+  delete temp->next;
+  temp->next = nullptr;
+}
+
+template <typename T>
+void deleteAtanyPos(ListNode<T>*& head,
+                    int pos) {  // pass by reference-here u want to
+                                // reflect the changes
+  if (head == nullptr) {
+    return;
+  }
+
+  ListNode<T>* temp = head;
+  if (pos <= 1) {
+    // you are deleting the first node
+    head = temp->next;
+    delete temp;
+    return;
+  }
+
+  for (int i = 1; i < pos - 1 & temp->next != nullptr; i++) {
+    temp = temp->next;
+  }
+  // if you are here, we are either at pos-1 or the pos doesnt exist(you are
+  // last node)
+  // check is it is scenario 2
+  if (temp->next = nullptr) {
+    // you are at the last node , the given pos doesn't exist
+    return;
+  }
+  // you are at pos-1
+  ListNode<T>* toDelete = temp->next;
+  temp->next = toDelete->next;
+  delete toDelete;
+}
+
+template <typename T>
+void deleteAtByVal(ListNode<T>*& head,
+                   T data) {  // delete the first occurence of this value in LL
+  if (head == nullptr) {
+    return;
+  }
+
+  ListNode<T>* temp = head;
+  // the value is present at the head
+  if (head->val = data) {
+    // you are deleting the first node
+    head = temp->next;
+    delete temp;
+    return;
+  }
+
+  // traverse till pos-1
+  while (temp->next != nullptr) {
+    if (temp->next->val == data) {
+      // you are the node whose next node's val matched with data
+      ListNode<T>* toDelete = temp;
+      temp->next = toDelete->next;
+      delete toDelete;
+      return;
+    }
+    temp = temp->next;
+  }
+  // you are at the last node
+  // the value u r looking for is not there in the current LL
+  return;
+}
+
+// Section 02- insertion,deletion
+template <typename T>
+void printLL(ListNode<T>*& head) {
+  ListNode<int>* temp = head;
+  while (temp != nullptr) {
+    cout << temp->val << "-->";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
+}
+// searching iteratively
+template <typename T>
+bool searchInLL(ListNode<T>*& head, T key) {
+  ListNode<int>* temp = head;
+  while (temp != nullptr) {
+    if (temp->val == key) {
+      return true;
+    }
+    temp = temp->next;
+  }
+  return false;
+}
+
+// searching recursively
+template <typename T>
+bool searchInLLRecursively(ListNode<T>* start, T key) {
+  if (start == nullptr) {
+    return false;
+  }
+  // current state f(i,key)
+  if (start == nullptr) {
+    return true;
+  }
+  start = start->next;
+  return searchInLLRecursively(start, key);
+}
+
+// length
+template <typename T>
+int lengthLL(ListNode<T>*& head) {
+  ListNode<T>* temp = head;
+  int count = 0;
+
+  while (temp != nullptr) {
+    count++;
+    temp = temp->next;
+  }
+  return count;
 }
